f582ef84d92411555c3d9b1862260d4dbef3471e
thiepn <junsosmart@gmail.com>
2026-08-22 23:30:51 +0200

    Unify expanded tool catalogs for discovery

---
+++ src/app/allToolCatalog.ts
@@ -0,0 +1,8 @@
+import { E4_TOOL_CATALOG } from './e4ToolCatalog';
+import { E5_TOOL_CATALOG } from './e5ToolCatalog';
+import { E6_TOOL_CATALOG } from './e6ToolCatalog';
+import { TOOL_CATALOG, type ToolCatalogItem } from './toolCatalog';
+
+export const ALL_TOOL_CATALOG:ToolCatalogItem[]=[...TOOL_CATALOG,...E4_TOOL_CATALOG,...E5_TOOL_CATALOG,...E6_TOOL_CATALOG];
+
+export function findAllTool(id:string):ToolCatalogItem|undefined{return ALL_TOOL_CATALOG.find(tool=>tool.id===id);}
