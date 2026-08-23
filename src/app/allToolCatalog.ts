386e958a0405cf026134d36a7ca4f09986a27f24
thiepn <junsosmart@gmail.com>
2026-08-24 00:39:56 +0200

    E11: add proof tools to global catalog

--- src/app/allToolCatalog.ts
+++ src/app/allToolCatalog.ts
@@ -5,8 +5,9 @@ import { E7_TOOL_CATALOG } from './e7ToolCatalog';
 import { E8_TOOL_CATALOG } from './e8ToolCatalog';
 import { E9_TOOL_CATALOG } from './e9ToolCatalog';
 import { E10_TOOL_CATALOG } from './e10ToolCatalog';
+import { E11_TOOL_CATALOG } from './e11ToolCatalog';
 import { TOOL_CATALOG, type ToolCatalogItem } from './toolCatalog';
 
-export const ALL_TOOL_CATALOG:ToolCatalogItem[]=[...TOOL_CATALOG,...E4_TOOL_CATALOG,...E5_TOOL_CATALOG,...E6_TOOL_CATALOG,...E7_TOOL_CATALOG,...E8_TOOL_CATALOG,...E9_TOOL_CATALOG,...E10_TOOL_CATALOG];
+export const ALL_TOOL_CATALOG:ToolCatalogItem[]=[...TOOL_CATALOG,...E4_TOOL_CATALOG,...E5_TOOL_CATALOG,...E6_TOOL_CATALOG,...E7_TOOL_CATALOG,...E8_TOOL_CATALOG,...E9_TOOL_CATALOG,...E10_TOOL_CATALOG,...E11_TOOL_CATALOG];
 
 export function findAllTool(id:string):ToolCatalogItem|undefined{return ALL_TOOL_CATALOG.find(tool=>tool.id===id);}
