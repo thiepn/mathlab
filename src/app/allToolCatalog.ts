e1af9a43f86cb76f9e495e4c5807390cc5aedfe5
thiepn <junsosmart@gmail.com>
2026-08-23 22:18:27 +0200

    Add E9 to global tool discovery

--- src/app/allToolCatalog.ts
+++ src/app/allToolCatalog.ts
@@ -3,8 +3,9 @@ import { E5_TOOL_CATALOG } from './e5ToolCatalog';
 import { E6_TOOL_CATALOG } from './e6ToolCatalog';
 import { E7_TOOL_CATALOG } from './e7ToolCatalog';
 import { E8_TOOL_CATALOG } from './e8ToolCatalog';
+import { E9_TOOL_CATALOG } from './e9ToolCatalog';
 import { TOOL_CATALOG, type ToolCatalogItem } from './toolCatalog';
 
-export const ALL_TOOL_CATALOG:ToolCatalogItem[]=[...TOOL_CATALOG,...E4_TOOL_CATALOG,...E5_TOOL_CATALOG,...E6_TOOL_CATALOG,...E7_TOOL_CATALOG,...E8_TOOL_CATALOG];
+export const ALL_TOOL_CATALOG:ToolCatalogItem[]=[...TOOL_CATALOG,...E4_TOOL_CATALOG,...E5_TOOL_CATALOG,...E6_TOOL_CATALOG,...E7_TOOL_CATALOG,...E8_TOOL_CATALOG,...E9_TOOL_CATALOG];
 
 export function findAllTool(id:string):ToolCatalogItem|undefined{return ALL_TOOL_CATALOG.find(tool=>tool.id===id);}
